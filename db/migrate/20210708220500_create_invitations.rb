eca36c65b2ee5f3d1dcfef5591df066aee9f914e
angelacuahutle <angela.cuahutle@gmail.com>
2021-07-08 18:59:38 -0500

    add association in the models between invitation, user and event

---
+++ db/migrate/20210708220500_create_invitations.rb
@@ -0,0 +1,9 @@
+class CreateInvitations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :invitations do |t|
+      t.belongs_to :creator, null: false, foreign_key: {to_table: :users}
+      t.belongs_to :event
+      t.timestamps
+    end
+  end
+end
