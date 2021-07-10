c7cd7a5eaff480555cedee75dd21cfca5e87ec7b
angelacuahutle <angela.cuahutle@gmail.com>
2021-07-10 18:17:54 -0500

    fix linter errors

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,3 +1,2 @@
 class InvitationsController < ApplicationController
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    events = current_user.events.all
+    @events = current_user.events.all
   end
 end
