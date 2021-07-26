d1f058438ed3963c0f9ce9e367d9af339da088db
angelacuahutle <angela.cuahutle@gmail.com>
2021-07-25 21:00:26 -0500

    fix synthax

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,12 +28,11 @@ class EventsController < ApplicationController
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        end
-        event = current_user.created_events.new
       end
+      @event = current_user.created_events.new
     end
   end
-
+  
   # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
@@ -44,7 +43,7 @@ class EventsController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
@@ -53,14 +52,14 @@ class EventsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-
+  
   # Use callbacks to share common setup or constraints between actions.
   def set_event
     @event = Event.find(params[:id])
   end
-
+  
   # Only allow a list of trusted parameters through.
   def event_params
     params.fetch(:event, {}).permit(:title)
